77b67efbc3df81bce20e8bf1a516fbe97e7a2bae
Fabiane C dos Santos <98193750+FabiaAz@users.noreply.github.com>
2024-07-29 20:12:10 -0300

    Exercicio2 do desafio1 resolvido.

--- e_2/e_2.py
+++ e_2/e_2.py
@@ -1,1 +1,7 @@
-teste2
+Exercicio 2. Peça ao usuário para informar o ano de nascimento. Em seguida,
+calcule e imprima a idade atual.
+
+ano_de_nascimento = int(input('Digita o ano do seu nascimento'))
+ano_atual = 2024
+idade_atual = ano_atual - ano_de_nascimento
+print(f'Você tem {idade_atual} anos')
