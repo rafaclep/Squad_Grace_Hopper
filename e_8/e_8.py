cce545039e9ce2fbe09dbecc79da16936ee55b3a
Daiany Morais <141277776+DayMorais@users.noreply.github.com>
2024-07-30 15:33:46 -0300

    Update e_8.py

---
+++ e_8/e_8.py
@@ -0,0 +1,6 @@
+horasSemana = float(input("Digite a quantidade de horas que você realiza atividade física na semana: "))
+caloriasMinuto = 5
+
+caloriasMes = ((horasSemana * 4) * 60)*5
+
+print(f"O total de calorias queimadas em um mês é: {caloriasMes}.")
