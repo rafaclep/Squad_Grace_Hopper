e736a52a588b064e1fad7dab58d435ed6928ecf4
MaylaSilva <85751607+MaylaSilva@users.noreply.github.com>
2024-07-29 22:12:55 -0300

    Desafio 2 squad grace hopper (#4)
    * Correções Exercicio 1 do desafio 2

---
+++ e_01/e_1.py
@@ -0,0 +1,12 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+
+print ("Vamos descobrir qual é maior número")
+primeiroNumero = int(input("Digite o primeiro número: "))
+segundoNumero = int(input("Digite o segundo número: "))
+
+if primeiroNumero > segundoNumero:
+    print("O primeiro número digitado é o maior!")
+elif segundoNumero > primeiroNumero:
+    print("O segundo número digitado é o maior!")
+else:
+    print("São o mesmo número")
