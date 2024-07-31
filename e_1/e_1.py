b8bb193d7724bc2de4b898ea949bea58812d1dec
Fabiane C dos Santos <98193750+FabiaAz@users.noreply.github.com>
2024-07-31 15:17:02 -0300

    Exercicio1 do desafio4

---
+++ e_1/e_1.py
@@ -0,0 +1,14 @@
+"""
+
+Faça um programa, com uma função que necessite de três
+argumentos, e que forneça a soma desses três argumentos.
+
+"""
+
+
+def soma_tres_numeros(a, b, c):
+    return a + b + c
+
+
+resultado = soma_tres_numeros(10, 20, 30)
+print(f"A soma dos três números é: {resultado}")
