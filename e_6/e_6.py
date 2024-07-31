f1a8dcfcd660ef4fdc4bdad09b950f75c3da53c5
Thaysa Borges <115192383+ThayBorges@users.noreply.github.com>
2024-07-31 10:14:10 -0300

    Finaliza exercício 06 do desafio 02

--- e_6/e_6.py
+++ e_6/e_6.py
@@ -1,1 +1,19 @@
-teste
+'''6. Crie um programa que solicite ao usuário um login e uma senha. O
+programa deve permitir o acesso apenas se o usuário for "admin" e a senha
+for "admin123", caso contrário imprima uma mensagem de erro.
+'''
+def verificar_acesso(login, senha):
+    if login == "admin" and senha == "admin123":
+        return True
+    else:
+        return False
+
+# Solicita ao usuário para inserir o login e a senha
+login_usuario = input("Digite o login: ")
+senha_usuario = input("Digite a senha: ")
+
+# Verifica se o acesso é permitido
+if verificar_acesso(login_usuario, senha_usuario):
+    print("Acesso permitido.")
+else:
+    print("Erro: Login ou senha incorretos.")
